30dfc13052a415d0deca58df898b8c2ab9b6c937
raksh <rakshitha.r@modernchipsolutions.com>
2021-12-10 16:52:09 +0530

    10 DEC 2021

---
+++ _12_Python_Assignment/_01_Important_Interviev_questions.py
@@ -0,0 +1,166 @@
+'''Important Questions'''
+"""How python interpreter works
+1.The interpreter reads a python code or instruction. Then it verifies that the instruction is well formatted
+2.If there is no error, i.e. if the python instruction or code is well formatted then the interpreter translates 
+it into its equivalent form in intermediate language 
+3.Byte code is sent to the Python Virtual Machine (PVM).Here again the byte code is executed on PVM.If an error occurs during
+ this execution then the execution ...
+ 
+ **Python features
+ 
+ 1) Easy to Learn and Use
+
+    Python is easy to learn as compared to other programming languages. Its syntax is straightforward and much the same as
+    the English language. There is no use of the semicolon or curly-bracket, the indentation defines the code block. It is the recommended programming language for beginners.
+
+2) Expressive Language
+    Python can perform complex tasks using a few lines of code. A simple example, the hello world program you simply type
+    print("Hello World"). It will take only one line to execute, while Java or C takes multiple lines.
+
+3) Interpreted Language
+    Python is an interpreted language; it means the Python program is executed one line at a time. The advantage of
+    being interpreted language, it makes debugging easy and portable.
+
+4) Cross-platform Language
+  Python can run equally on different platforms such as Windows, Linux, UNIX, and Macintosh, etc. So, we can say that 
+  Python is a portable language. It enables programmers to develop the software for several competing platforms by writing
+  a program only once.
+
+5) Free and Open Source
+  Python is freely available for everyone. It is freely available on its official website www.python.org
+. It has a large community across the world that is dedicatedly working towards make new python modules and functions. 
+  Anyone can contribute to the Python community. The open-source means, "Anyone can download its source code without paying 
+  any penny."
+
+6) Object-Oriented Language
+   Python supports object-oriented language and concepts of classes and objects come into existence. It supports inheritance,
+   polymorphism, and encapsulation, etc. The object-oriented procedure helps to programmer to write reusable code and 
+   develop applications in less code.
+
+7) Extensible
+   It implies that other languages such as C/C++ can be used to compile the code and thus it can be used further in our 
+   Python code. It converts the program into byte code, and any platform can use that byte code.
+
+8) Large Standard Library
+    It provides a vast range of libraries for the various fields such as machine learning, web developer, and also 
+    for the scripting. There are various machine learning libraries, such as Tensor flow, Pandas, Numpy, Keras, and Pytorch,
+    etc. Django, flask, pyramids are the popular framework for Python web development.
+
+9) GUI Programming Support
+   Graphical User Interface is used for the developing Desktop application. PyQT5, Tkinter, Kivy are the libraries which 
+   are used for developing the web application.
+
+10) Integrated
+   It can be easily integrated with languages like C, C++, and JAVA, etc. Python runs code line by line like C,C++ Java.
+   It makes easy to debug the code.
+
+11. Embeddable
+   The code of the other programming language can use in the Python source code. We can use Python source code in another 
+   programming language as well. It can embed other language into our code.
+
+12. Dynamic Memory Allocation
+In Python, we don't need to specify the data-type of the variable. When we assign some value to the variable, it automatically 
+allocates the memory to the variable at run time. Suppose we are assigned integer value 15 to x, then we don't need to
+write int x = 15. Just write x = 15.
+
+ **Interpretor
+ Python is an interpreted language; it means the Python program is executed one line at a time. The advantage of being 
+ interpreted language, it makes debugging easy and portable.
+ 
+ **Flavours of python
+ 
+1.CPython : CPython is the Python compiler implemented in C programming language. In this, Python code is internally converted 
+into the byte code using standard C functions. ...
+
+2.Jython : Earlier known as JPython. Jython is an implementation of the Python programming language designed to run on the
+ Java platform. ...
+ 
+3.PyPy : This is the implementation using Python language. PyPy often runs faster than CPython because PyPy is a just-in-time 
+compiler while CPython is an interpreter.
+
+4.IronPython : IronPython is an open-source implementation of the Python programming language which is tightly integrated
+ with the .NET Framework.
+ 
+5.RubyPython : RubyPython is a bridge between the Ruby and Python interpreters. It embeds a Python interpreter in
+ the Ruby application’s process using FFI (Foreign Function Interface).
+ 
+ **Dynamically typed programming language
+ Python is a dynamically typed language. This means that the Python interpreter does type checking only as code runs, and the 
+ type of a variable is allowed to change over its lifetime. Here are a couple of examples that demonstrate those ideas: In the 
+ first example, the branch 1 + "two" never runs, so it’s never type checked.
+ 
+ **procedure oriented v/s object oriented
+ 
+ Following are the important differences between Procedural Oriented Programming (POP) and Object Oriented Programming (OOP)
+
+
+**PYTHON IDEs
+
+   There are many free and commercial IDEs available for Python. Here, we will learn how to use some open-source editors
+    to execute Python scripts or statements.
+
+1.Jupyter Notebook
+
+ The Jupyter Notebook is a browser-based graphical interface to the IPython shell. It allows the user to include formatted text, 
+ static and dynamic visualizations, mathematical equations, JavaScript widgets, etc. along with the Python code. The Jupyter
+ Notebook document can be exported to PDF, Python script, or HTML.
+ By default, the IPython kernel drives the Jupyter Notebook application. However, it supports other languages like Julia 
+ and R. (Jupyter stands for JUlia, PYThon, and R).
+
+2.Visual Studio Code
+
+   Visual Studio Code is an open-source IDE to develop different types of applications on Windows, Mac, and Linux platform. 
+   You can develop Python 3 applications by installing Python extension for VS Code from the Visual Studio Marketplace.
+
+3.Online Python Shell
+
+ Installing Python (or any software) can be a little daunting for a newbie. Fortunately there are many online resources 
+ to get familiar with the syntax, features and philosophy of Python before deciding to install Python in the local machine.
+  You can launch an online Python Shell directly from the official website - https://www.python.org/shell.
+  
+4.Pycharm
+ Available as a cross-platform application, PyCharm is compatible with Linux, macOS, and Windows platforms. Sitting gracefully among the best Python IDEs, PyCharm provides support for both Python 2 (2.7) and Python 3 (3.5 and above) versions.
+  PyCharm is an integrated development environment (IDE) used in computer programming, specifically for the Python language.
+  It is developed by the Czech company JetBrains (formerly known as IntelliJ).
+  
+**Automatic Garbage collection
+Python: Automatic Garbage collection Garbage collection in Python works automatically. When no reference is left to a 
+variable/data/value or object then the memory occupied by that object is freed up by garbage collection mechanism of python. 
+Thus, python provides with good memory management and prevents the wastage of memory.
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ """
---
+++ _12_Python_Assignment/_02_python_developer_keyskills.py
@@ -0,0 +1,83 @@
+"""   skills                                                     company
+
+1.Skilled in python,django ==                                       IBM(2y)
+
+
+2.Programming: Python, Java
+Data Processing Framework: Spark, Apache Beam
+Cloud: GCP, AWS
+Data Processing Platform: DataProc, DataFlow
+Scheduler: Airflow, Google Composer
+Data-Lake: BigQuery, RedShift
+RDBMS: PostgreSQL, CloudSQL
+NoSQL: MongoDB
+Cloud Storage: GCP Cloud Storage, AWS S3
+SearchEngine: ElasticSearch
+Framework: Flask,FastAPI                                                Inviz AI - Technology(5.2y)
+
+3.Skilled in Python (Programming Language), Unix and PL/SQL.            Cognizant Information Technology & Services(2.2y)
+
+4.Python Developer | Framework Developer |                                Oracle(3.2)
+
+5.Python Developer | Data Engineering | MongoDB Certified | Senior SDE    InViz AI(7y) and other company
+   REST API for Flask, FastAPI applications, Experience in building an
+   ETL pipeline for Data Warehouses, building data pipeline for data lakes.
+
+6.python,jango,js,bootstrap,rest api,jss,html,css                       Ameotech Informatic Information Technology & Services(3y)
+
+7.Skilled in Python ,shell scripting ,Jenkins, Software development      Bosch(2.8y)
+
+8.python,django,flask                                                     Way To Code Technologies LLP(1.5y)
+
+9.python,django                                                           google(1y)
+
+10.python,flask,django framework,php,js,frontend and backend technologies  Deltabee Technologies(2.3y)
+"""
+"""
+        Automation                                                                      company
+1. manual testing, selenium, core java, SQL,oops concepts,
+STLC,SDLC,agile methodologies, regression testing,                                      Cisco(2-3y)
+bug life cycle,test plan,test case, defect tracking,
+XPATH, locators, web elements,handling frames, pom,test Ng,joints
+
+2.Automation Test Engineer| Selenium | BDD | Appium | Rest Assured |
+WebDriverIO | Cypress | Python | Java | Jenkins | Docker | Machine Learning       mjunction services ltd(4y)
+
+3.SDDC | Python Automation | Virtualization | Test Automation Framework Development         Pluribus Networks(7y)
+
+4.Functional Consultant SAP FICO.                                                            Congnizent(5.5y)
+Worksoft Certify | Selenium | Leapwork
+
+5.Selenium automation with CI/CD delivery methods
+using Jenkins.automation BDD ,TDD                                                    infosys and congnizent(7.2y)
+TestNG
+
+EAgile and traditional waterfall method
+Restful APIs using Postman and SOAP UI tool. Scoping,
+Estimation, Resourcing, Planning, Defects/Issues/Risks Management and Delivery Management.
+
+6.Selenium | Java | JIRA                                                                             CGI,Boeing(3.8y)
+
+7. 1. PLC logic development using RSLogix500(AB), GX Developer, GX works2,
+GX IEC Developer(Mitsubishi), CX Programmer(Omron), WPLSoft2.42(Delta),
+ProProgII, ProProg III(Baumuller), PNOZmulti Configurator10.6(PILZ), TIA (Seimens)
+2. HMI development using DOP Soft2.00.7 (Delta), GT Designer3 (Mitsubishi),
+connected components Workbench (AB), NB Designer (Omron), V-SFT (FUJI).
+3. SERVO Drive and Motor Using MR Configurator2 (Mitsubishi), ASD Soft V5.03.01 (Delta),\
+  ProDrive, ProMaster (Baumuller), 
+5. Modbus, Ethernet, CCLink, EtherCAT, Profi-Net                                                     DiFACTO Robotics(5y)
+
+
+8.Selenium and API testing AWS,Rest Assured.Lambda.ECS.                                          Ness Technologies (India) Pvt. Ltd.
+                                                                                                 Wells Fargo
+                                                                                                  Mphasis(8y)
+9.Web Applications
+Test Automation                                                                                 HealthAsyst(4.9)
+Manual Testing
+Selenium Testing
+Testng
+Autoit
+
+10.SDLC,corejava,Selenium,AWS,GIT,html,xml                                                       Tcs,Oracle(3.7y)
+
+"""
