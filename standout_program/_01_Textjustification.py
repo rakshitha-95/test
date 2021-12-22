cdc2e000d6020e080f4953d975cc11cc250e79d2
raksh <rakshitha.r@modernchipsolutions.com>
2021-12-22 22:33:16 +0530

    22-DEC-2021

--- standout_program/_01_Textjustification.py
+++ standout_program/_01_Textjustification.py
@@ -6,48 +6,39 @@ If the number of spaces on a line does not divide evenly between words, the empt
 assigned more spaces than the slots on the right.For the last line of text, it should be left-justified and no
 extra space is inserted between words.'''
 
-def fullJustify(words, L):
-    result = []
+from typing import List
 
-    i = 0
-    while i < len(words):
-        # count words in one line
-        size, begin = 0, i
-        while i < len(words):
-            if size == 0:
-                newsize = len(words[i])
+
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        width_left = maxWidth
+        line = []
+        res = []
+        for i in words:
+            if width_left - len(i) >= 0:
+                line.append(i)
+                width_left -= (len(i) + 1)
             else:
-                newsize = size + len(words[i]) + 1
-            if newsize <= L:
-                size = newsize
-            else:
-                break
-            i += 1
+                width_left += 1
+                if len(line) > 1:
+                    extra_space = width_left // (len(line) - 1)
+                    space_rem = width_left % (len(line) - 1)
+                    for j in range(len(line) - 1):
+                        line[j] += (extra_space + 1) * " "
+                        if space_rem:
+                            line[j] += " "
+                            space_rem -= 1
+                else:
+                    line[0] = line[0] + " " * (maxWidth - len(line[0]))
+                res.append("".join(line))
+                line = [i]
+                width_left = maxWidth - (len(i) + 1)
+        for j in range(len(line) - 1):
+            line[j] += " "
+        last_line = "".join(line) + " " * (maxWidth - len("".join(line)))
+        res.append(last_line)
+        return res
 
-        # count space number
-        spaceCount = L - size
-        if i - begin - 1 > 0 and i < len(words):
-            everyCount = spaceCount / (i - begin - 1) + 1
-            spaceCount %= i - begin - 1
-        else:
-            everyCount = 1
 
-        # add space
-        j = begin
-        while j < i:
-            if j == begin:
-                s = words[j]
-            else:
-                s += ' ' * everyCount
-                if spaceCount > 0 and i < len(words):
-                    s += ' '
-                    spaceCount -= 1
-                s += words[j]
-            j += 1
-        s += ' ' * spaceCount
-        result.append(s)
-
-    return result
-words="#Easy-to-learn Python has few keywords, simple structure, and a clearly defined syntax. This allows the student to pick up the language quickly."
-L=15
-print(fullJustify(words, L))
+if __name__ == "__main__":
+    print(Solution().fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16))
